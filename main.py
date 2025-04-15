0230064a63210de339a1126a3a0f7e5c5f4927c3
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2025-04-15 11:12:08 +0200

    updated main py to point to correct build directory

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from backend.main import app as backend_app
 app = FastAPI()
 
 # Mount the Flutter web build
-app.mount("/", StaticFiles(directory="noabuddy_flutter_app/build/web", html=True), name="static")
+app.mount("/", StaticFiles(directory="noabuddy_heroku_server", html=True), name="static")
 
 # Mount the API under /api
 app.mount("/api", backend_app)
